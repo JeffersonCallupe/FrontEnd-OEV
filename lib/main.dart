0175b63675c73cbde7916f6eab1b1153b4e8e366
Jefferson J. <jeff.callupe@gmail.com>
2025-06-14 13:34:25 -0500

    initial and functional login

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:oev_mobile_app/config/constants/environment.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:oev_mobile_app/config/router/app_router.dart';
+import 'package:oev_mobile_app/presentation/screens/login/providers/auth_provider.dart';
+
+void main() async {
+  await Environment.initEnvironment();
+
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // ref.watch(authProvider.notifier).logout();
+    final appRouter = ref.watch(goRouterProvider);
+    return MaterialApp.router(
+      title: 'OEV App',
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
