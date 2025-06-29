26b54d5b00ee34d2913fab5e15788c5d70a364b8
carlos15b <carlossoller15@gmail.com>
2025-06-29 15:56:50 -0500

    Estructura de my_courses.dart

--- lib/presentation/screens/login/widgets/course/my_courses.dart
+++ lib/presentation/screens/login/widgets/course/my_courses.dart
@@ -11,3 +11,59 @@ import 'package:oev_mobile_app/presentation/screens/course/course_editable_conte
 
 final searchQueryProvider = StateProvider<String>((ref) => "");
 final showCompletedProvider = StateProvider<bool>((ref) => false);
+
+class MyCourses extends ConsumerWidget {
+  const MyCourses({super.key});
+ 
+@override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final colors = Theme.of(context).colorScheme;
+    final enrolledCoursesAsync = ref.watch(enrolledCoursesProvider);
+    final publishedCoursesAsync = ref.watch(coursesPublishedByInstructorProvider);
+    final searchQuery = ref.watch(searchQueryProvider);
+    final showCompleted = ref.watch(showCompletedProvider);
+    final loggedUser = ref.read(authProvider).token;
+
+    final bool isStudentOrAdmin = loggedUser!.role == 'STUDENT' || loggedUser.role == 'ADMINISTRATIVE';
+
+    return Column(
+      children: [
+        const SizedBox(height: 20),
+        const Text(
+          'Sección de cursos',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        const Text(
+          'Continúa donde lo dejaste',
+          style: TextStyle(color: Colors.white70),
+        ),
+        const SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(8, 16, 8, 8),
+          child: SizedBox(
+            width: 420,
+            child: TextField(
+              cursorColor: colors.primary,
+              onChanged: (value) {
+                ref.read(searchQueryProvider.notifier).state = value;
+              },
+              style: const TextStyle(color: Colors.white),
+              onTapOutside: (event) => FocusScope.of(context).unfocus(),
+              decoration: const InputDecoration(
+                hintText: 'Buscar por curso',
+                hintStyle: TextStyle(color: Colors.grey),
+                prefixIcon: Icon(Icons.search),
+                filled: true,
+                fillColor: Color(0xff343646),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                ),
+              ),
+            ),
+          ),
+        ),
+
+
+
+
+}
