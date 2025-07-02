f78ddbb8bb8464dd25fd1a95bdfd2e3c34f8ebba
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2025-07-02 09:53:26 -0500

    refactor: update CourseCard to navigate to CourseEditableContent for admins

--- lib/presentation/widgets/course/course_card.dart
+++ lib/presentation/widgets/course/course_card.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oev_mobile_app/domain/entities/course/course_model.dart';
 import 'package:oev_mobile_app/presentation/providers/auth_provider.dart';
+import 'package:oev_mobile_app/presentation/screens/course/course_editable_content.dart';
+import 'package:oev_mobile_app/presentation/widgets/course/course_detail.dart';
 
 class CourseCard extends ConsumerWidget {
   final Course course;
@@ -11,7 +13,17 @@ class CourseCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final loggedUser = ref.read(authProvider).token;
 
+    final isAdmin = loggedUser?.role == 'ADMIN';
+
     return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => isAdmin ? CourseEditableContent(course: course) : CourseDetailPage(courseId: course.id),
+          ),
+        );
+      },
       child: Container(
         decoration: BoxDecoration(
           color: const Color(0xFF32343E),
@@ -39,24 +51,16 @@ class CourseCard extends ConsumerWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(course.name,
-                      style: const TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white)),
+                  Text(course.name, style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white)),
                   const SizedBox(height: 4),
-                  Text(course.instructorName,
-                      style:
-                          const TextStyle(color: Colors.white70, fontSize: 12)),
+                  Text(course.instructorName, style: const TextStyle(color: Colors.white70, fontSize: 12)),
                   const SizedBox(height: 4),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       const Icon(Icons.people, size: 16, color: Colors.white70),
                       const SizedBox(width: 4),
-                      Text(course.totalStudents.toString(),
-                          style: const TextStyle(
-                              color: Colors.white, fontSize: 12)),
+                      Text(course.totalStudents.toString(), style: const TextStyle(color: Colors.white, fontSize: 12)),
                     ],
                   ),
                 ],
