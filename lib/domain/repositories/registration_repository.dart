2653af33a090dd608cfee8646cc8acf5ef4a3408
Jefferson J. <jeff.callupe@gmail.com>
2025-07-03 10:47:35 -0500

    update branch

--- lib/domain/repositories/registration_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:oev_mobile_app/domain/entities/dto/conference_registration.dart';
-
-abstract class RegistrationRepository {
-  Future<void> createRegistration(int userId, int conferenceId);
-  Future<void> deleteRegistration(int registrationId);
-  Future<List<Map<String, dynamic>>> findRegisteredUsersByConferenceId(int conferenceId);
-
-  Future<List<ConferenceRegistration>> getRegistrationsByUserId(int userId);
-}
